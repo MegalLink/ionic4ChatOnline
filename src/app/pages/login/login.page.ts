b88431980968c0c078687cb8842daced5ef34835
MegalLink <50426219+MegalLink@users.noreply.github.com>
2019-08-23 19:32:33 -0500

    Creacion auth Service y login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private authS:AuthService, public router:Router) { }
+ emailTs: string;
+ passwordTs: string;
   ngOnInit() {
   }
+onSubmitLogin(){
+  this.authS.login(this.emailTs,this.passwordTs).then(respuesta=>{
+ this.router.navigate(['/home']);
+  }).catch(error=>alert('Los datos son incorrectos o no existe el usuario'));
+}
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import {AngularFireAuth} from '@angular/fire/auth';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth:AngularFireAuth) { }
+
+login(mail,pass){
+  return new Promise((aceptado,rechazado)=>{
+    this.afAuth.auth.signInWithEmailAndPassword(mail,pass).then(respuesta=>{
+      aceptado(respuesta.user)
+    }).catch(error=>rechazado(error));
+  
+  })
+
+  
+}
+
 }

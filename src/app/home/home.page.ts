19a076a91eb47c2912664e7760a187d388c2b2a5
MegalLink <50426219+MegalLink@users.noreply.github.com>
2019-08-25 15:34:37 -0500

    Log Out con menu de opciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -39,41 +39,18 @@ this.modal.create({
 }).then((modal)=>modal.present()); //cuando el modal este creado lo presenta
 }
 async presentActionSheet() {
-  const actionSheet = await this.actionSheetController.create({
-    header: 'Albums',
+  const actionSheet = await this.actionSheetController.create(
+    {
+    header: 'Opciones',
     buttons: [{
-      text: 'Delete',
+      text: 'Desconectarse',
       role: 'destructive',
-      icon: 'trash',
-      handler: () => {
-        console.log('Delete clicked');
+      icon: 'log-out',
+      handler: () => { //Desencade = Trigger
+        this.onLogOut();
       }
-    }, {
-      text: 'Share',
-      icon: 'share',
-      handler: () => {
-        console.log('Share clicked');
-      }
-    }, {
-      text: 'Play (open modal)',
-      icon: 'arrow-dropright-circle',
-      handler: () => {
-        console.log('Play clicked');
-      }
-    }, {
-      text: 'Favorite',
-      icon: 'heart',
-      handler: () => {
-        console.log('Favorite clicked');
-      }
-    }, {
-      text: 'Cancel',
-      icon: 'close',
-      role: 'cancel',
-      handler: () => {
-        console.log('Cancel clicked');
-      }
-    }]
+    }
+  ]
   });
   await actionSheet.present();
 }
